589ddbefb22b03709d8fa632ec565a3b57d2090a
jesus rodriguez <jr433587@gmail.com>
2024-06-13 17:59:49 -0600

    segundo commit 2

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const express = require("express")
+const express = require("express");
+const morgan = require("morgan");
 const app = express()
 const port = 3500
+const comidaRouter = require('./routes/comidaRouter')
 
-app.get("/", (req, res) => {
-    res.json({mensaje:"Aplicacion funcionando"})
-})
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+app.use('/API', comidaRouter)
+
+app.use((req,res) => {
+    res.status(404).json({message: 'Ruta no encontrada'});
+});
+
+
 
 app.listen(port,()=>{
     console.log( `Aplicacion corriendo en el puerto ${port} `)
